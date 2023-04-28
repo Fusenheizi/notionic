3f135d895eeeed7f0ef129bb0a31216f8afcaca8
Fusenheizi <fusenheizi@qq.com>
2023-04-28 18:47:10 +0800

    Update layout.js

--- layouts/layout.js
+++ layouts/layout.js
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion'
 import Container from '@/components/Container'
 import Content from '@/components/Post/Content'
 import Aside from '@/components/Post/Aside'
-import Comments from '@/components/Post/Comments'
+import Comments from '@/components/Post/Comments' 
 import PostFooter from '@/components/Post/PostFooter'
 
 const Layout = ({ blockMap, frontMatter, fullWidth = false, subPage = false }) => {
